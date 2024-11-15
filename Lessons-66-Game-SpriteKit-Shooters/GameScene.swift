feae77c6fd8e50a917b2895b96754300070e2c1d
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-15 14:58:16 +0200

    start

--- Lessons-66-Game-SpriteKit-Shooters/GameScene.swift
+++ Lessons-66-Game-SpriteKit-Shooters/GameScene.swift
@@ -7,82 +7,75 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var bulletsSprite: SKSpriteNode!   // патроны
+
+        var bulletTextures = [
+            SKTexture(imageNamed: "shots0"),
+            SKTexture(imageNamed: "shots1"),
+            SKTexture(imageNamed: "shots2"),
+            SKTexture(imageNamed: "shots3"),
+        ]
+    var bulletsInClip = 3 {
+            didSet {
+                bulletsSprite.texture = bulletTextures[bulletsInClip]
+            }
+        }
+    
+    var scoreLabel: SKLabelNode!
+
+        var score = 0 {
+            didSet {
+                scoreLabel.text = "Score: \(score)"
+            }
+        }
+    
+    var isGameOver = false
+    
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // Получаем размеры экрана
+               let screenSize = view.bounds.size
+        print(screenSize)
+     
+        //createBackground()
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let background = SKSpriteNode(imageNamed: "wood-background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        addChild(background)
+
+        let grass = SKSpriteNode(imageNamed: "grass-trees")
+        grass.position = CGPoint(x: 512, y: 50)
+        addChild(grass)
+        grass.zPosition = 100
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+//        let background = SKSpriteNode(imageNamed: "whackBackground")
+//        background.position = CGPoint(x: 512, y: 384)
+//        background.blendMode = .replace
+//        background.zPosition = -1
+//        addChild(background)
     }
     
+    func createBackground() {
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+        
+//            let background = SKSpriteNode(imageNamed: "wood-background")
+//            background.position = CGPoint(x: 512, y: 384)
+//            background.blendMode = .replace
+//           // background.size = screenSize
+//            addChild(background)
+//
+//            let grass = SKSpriteNode(imageNamed: "grass-trees")
+//            grass.position = CGPoint(x: 512, y: 384)
+//            addChild(grass)
+//            grass.zPosition = 100
         }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
